8c4f187c0bf955ab39ce250889107dfb63b5deed
Fan Chou <Chou.Fan@igt.com>
2015-10-09 15:47:30 +0800

    5.5.2 done.

--- Unity-ParticleSystem-5.0/Assets/_Scripts/Sprinkler.cs
+++ Unity-ParticleSystem-5.0/Assets/_Scripts/Sprinkler.cs
@@ -4,6 +4,10 @@ using System.Collections;
 public class Sprinkler : MonoBehaviour {
 	private float heightAboveFloor;
 	private ParticleSystem[] sprinklers;
+
+	private ParticleCollisionEvent[][] collisionEvents;
+	private GameObject fireBarrel;
+	private Fire fire;
 	// Use this for initialization
 	void Awake () {
 		sprinklers = GetComponentsInChildren<ParticleSystem>();
@@ -11,8 +15,38 @@ public class Sprinkler : MonoBehaviour {
 
 	void Start () {
 		heightAboveFloor = transform.position.y;
+
+		collisionEvents = new ParticleCollisionEvent[sprinklers.Length][];
+		
+		fireBarrel = GameObject.FindWithTag ("FireBarrel");
+		
+		fire = fireBarrel.GetComponent<Fire>();
 	}
-	
+
+	void OnParticleCollision (GameObject other){
+		if (other.tag == "FireBarrel") {
+			for (int i = 0; i < collisionEvents.Length; i++) {
+				collisionEvents[i] = new ParticleCollisionEvent[sprinklers[i].GetSafeCollisionEventSize()];
+				sprinklers[i].GetCollisionEvents(gameObject, collisionEvents[i]);
+				
+				for (int j = 0; j < collisionEvents[i].Length; j++) {
+					foreach (var ph in fire.particles) {
+						if (ph.varyAlpha) {
+							ph.DecreaseAlpha();
+						}
+						if (ph.varyEmission) {
+							ph.DecreaseEmission ();
+						}
+						if (ph.varyIntensity) {
+							ph.DecreaseIntensity ();
+						}
+						if (ph.varyRange) {
+							ph.DecreaseRange ();
+						}
+					}					}
+			}
+		}
+	}
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButton (0)) {
